7f466890aab413e09567aedc4633d1515cc106f6
ujwalsharma1 <122802951+ujwalsharma1@users.noreply.github.com>
2026-09-19 18:05:21 +0530

    Add getChannelProfile endpoint to fetch channel details and subscription status

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -368,6 +368,78 @@ const updateUserCoverImage = asyncHandler( async(req,res)=> {
   )
 })
 
+const getChannelProfile = asyncHandler(async(req,res)=> {
+
+  const {username} = req.params;
+
+  if(!username.trim()){
+    throw new ApiError(400, "username is missing");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match :{
+        username: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscriberCount: {
+          $size : "$subscribers"
+        },
+        channelSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond : {
+            if : {$in : [req.user?._id, "$subscribers.subscriber"]},
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        fullName: 1,
+        username: 1,
+        email: 1,
+        subscriberCount: 1,
+        channelSubscribedToCount: 1,
+        isSubscribed: 1, 
+        avatar: 1,
+        coverImage: 1
+      }
+    }
+  ])
+
+  if(!channel?.length){
+    throw new ApiError(404 , "channel does not exist")
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200 , channel[0] , "User channel fetched successfully")
+  )
+})
+
 export { registerUser, 
   loginUser, 
   logoutUser , 
@@ -376,4 +448,6 @@ export { registerUser,
   getCurrentUser , 
   updateAccountDetails , 
   updateUserAvatar , 
-  updateUserCoverImage};
+  updateUserCoverImage,
+  getChannelProfile
+}
